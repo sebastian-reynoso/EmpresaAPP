0ab63a2e2b0546b0806208d97cdd3704386c4271
Sebastian Reynoso <sreynoso0499@gmail.com>
2020-02-24 19:04:11 -0500

    Login Added

---
+++ EmpresaWEB/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using EmpresaWEB.Models;
+
+namespace EmpresaWEB.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Send(mvcLogin login)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Login/authentication", login).Result;
+            return View(response);
+        }
+    }
+}   
---
+++ EmpresaWEB/Models/mvcLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmpresaWEB.Models
+{
+    public class mvcLogin
+    {
+        [DisplayName("Correo")]
+        public string Correo { get; set; }
+
+        [DisplayName("Contraseña")]
+        public string Contraseña { get; set; }
+    }
+}
